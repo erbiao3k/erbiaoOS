5d38e047af150537fe236cfd083a6042c0a26f11
erbiao3k <1041876270@qq.com>
2022-04-19 21:46:12 +0800

    kube-proxy 配置初始化 20220419 21:41

--- initialize/kube_proxy.go
+++ initialize/kube_proxy.go
@@ -7,6 +7,14 @@ import (
 	"strings"
 )
 
+// KubeProxyCfg 生成kube-proxy配置文件
+func KubeProxyCfg(dir string, IPs []string) {
+	for _, ip := range IPs {
+		cfg := strings.ReplaceAll(customConst.KubeProxyCfg, "currentKubeproxyIP", ip)
+		file.Create(dir+ip+"-kube-proxy", cfg)
+	}
+}
+
 // KubeProxySystemdScript 生成kube-proxy systemd管理脚本
 func KubeProxySystemdScript(dir string, hosts []setting.HostInfo) {
 	for _, host := range hosts {

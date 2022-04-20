9ba9c32c6be6e5e2dcff7ba0580ad2610f7e4699
erbiao <1041876270@qq.com>
2022-04-20 15:04:47 +0800

    初始化calico网络组件编排文件，初始化coreDNS组件编排文件，初始化nginx主配置文件，以及nginx systemd管理脚本

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	customConst "erbiaoOS/const"
 	"erbiaoOS/initialize"
+	"erbiaoOS/pkg/file"
 	"erbiaoOS/setting"
 	"log"
 )
@@ -13,8 +14,8 @@ func main() {
 
 	log.Println("=+=+=+=+=+=+=+=+=+=+==+=++=+=+初始化配置文件==+=++=+=+=+=+=+=+=+=+=+=+=+=+=+=+")
 	clusterHost := setting.InitclusterHost(configDir)
-	//component := setting.ComponentContent(configDir)
-	// 初始化信息
+	component := setting.ComponentContent(configDir)
+
 	var k8sMasterHost []string
 	for _, master := range clusterHost.K8sMaster {
 		k8sMasterHost = append(k8sMasterHost, master.LanIp)
@@ -28,37 +29,37 @@ func main() {
 	var k8sClusterHost []string
 	k8sClusterHost = append(k8sClusterHost, k8sMasterHost...)
 	k8sClusterHost = append(k8sClusterHost, k8sNodeHost...)
-	//
-	//log.Println("=+=+=+=+=+=+=+=+=+=+=+创建k8s集群节点环境初始化脚本=+=+=+=+=+=+=+=+=+=+=+=+=+")
-	//for script, cmd := range customConst.InitScript {
-	//	file.Create(customConst.InitScriptDir+script, cmd)
-	//}
-	//
-	//log.Println("=+=+=+=+=+=+=+=+=+=+=+=+=+开始下载k8s必要组件,并分拣二进制文件=+=+=+=+=+=+=+=+=+")
-	//initialize.ComponentInit(component)
-	//
-	//log.Println("=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+初始化各组件证书=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+")
-	//
-	//log.Println("初始化CA机构证书")
-	//initialize.CaCert()
-	//
-	//log.Println("依据CA机构证书生成ETCD服务证书")
-	//initialize.EtcdCert(k8sMasterHost)
-	//
-	//log.Println("依据CA机构证书生成kube-apiserver证书")
-	//initialize.KubeApiserverCert(k8sMasterHost)
-	//
-	//log.Println("依据CA机构证书生成kube-proxy证书")
-	//initialize.KubeProxyCert()
-	//
-	//log.Println("依据CA机构证书生成kubectl管理证书")
-	//initialize.KubectlCert()
-	//
-	//log.Println("依据CA机构证书生成kube-controller-manager证书")
-	//initialize.KubeControllerManagerCert(k8sMasterHost)
-	//
-	//log.Println("依据CA机构证书生成kube-scheduler证书")
-	//initialize.KubeSchedulerCert(k8sMasterHost)
+
+	log.Println("=+=+=+=+=+=+=+=+=+=+=+创建k8s集群节点环境初始化脚本=+=+=+=+=+=+=+=+=+=+=+=+=+")
+	for script, cmd := range customConst.InitScript {
+		file.Create(customConst.InitScriptDir+script, cmd)
+	}
+
+	log.Println("=+=+=+=+=+=+=+=+=+=+=+=+=+开始下载k8s必要组件,并分拣二进制文件=+=+=+=+=+=+=+=+=+")
+	initialize.ComponentInit(component)
+
+	log.Println("=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+初始化各组件证书=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+")
+
+	log.Println("初始化CA机构证书")
+	initialize.CaCert()
+
+	log.Println("依据CA机构证书生成ETCD服务证书")
+	initialize.EtcdCert(k8sMasterHost)
+
+	log.Println("依据CA机构证书生成kube-apiserver证书")
+	initialize.KubeApiserverCert(k8sMasterHost)
+
+	log.Println("依据CA机构证书生成kube-proxy证书")
+	initialize.KubeProxyCert()
+
+	log.Println("依据CA机构证书生成kubectl管理证书")
+	initialize.KubectlCert()
+
+	log.Println("依据CA机构证书生成kube-controller-manager证书")
+	initialize.KubeControllerManagerCert(k8sMasterHost)
+
+	log.Println("依据CA机构证书生成kube-scheduler证书")
+	initialize.KubeSchedulerCert(k8sMasterHost)
 
 	log.Println("=+=+=+=+=+=+=+==+=+=+=+=+=+=+初始化各组件配置文件=+=+=+=+=+=+=+=+=+=+=+=+=+")
 	log.Println("初始化etcd systemd管理脚本，etcdctl客户端指令")
